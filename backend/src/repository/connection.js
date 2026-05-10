6302bb247b107607ffafc34fa57b296f278ed2c3
Nayana Oliveira <nayanaheslley123@Gmail.com>
2026-05-09 22:00:07 -0300

    primeira versão do navy

--- backend/src/repository/connection.js
+++ backend/src/repository/connection.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    database: process.env.MYSQL_DB
-})
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT
+});
 
 console.log('Conexão com o banco de dados estabelecida com sucesso!');
 
