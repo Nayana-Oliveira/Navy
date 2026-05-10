e9e6cc02235320a65e8214ace377598e3c68a3a1
Nayana Oliveira <nayanaheslley123@Gmail.com>
2026-05-09 22:55:15 -0300

    ajustes no frontend

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,18 +1,26 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import adicionarRotas from './routes.js';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import adicionarRotas from "./routes.js";
 
 const api = express();
 
 api.use(express.json());
-api.use(cors());
 
-api.get('/', (req, resp) => {
-    resp.send('API Navy rodando!');
-})
+api.use(
+  cors({
+    origin: ["http://localhost:4200", process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-adicionarRotas(api)
+api.get("/", (req, resp) => {
+  resp.send("API Navy rodando!");
+});
+
+adicionarRotas(api);
 
 const porta = process.env.PORT || process.env.PORTA || 5010;
+
 api.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
