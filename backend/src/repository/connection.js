51ba513b2ba38fc155997dcaf256662e7a34f6b5
Nayana Oliveira <nayanaheslley123@Gmail.com>
2026-05-09 22:25:22 -0300

    ajustando conexão

--- backend/src/repository/connection.js
+++ backend/src/repository/connection.js
@@ -1,13 +1,16 @@
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PWD,
   database: process.env.MYSQL_DB,
-  port: process.env.MYSQL_PORT
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-console.log('Conexão com o banco de dados estabelecida com sucesso!');
+console.log('Pool de conexão com o banco criado com sucesso!');
 
 export default connection;
